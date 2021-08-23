8c7b0c197d88ea4c1d8a02bd87885f53f48ef448
Dante Klaus <danteklausdev@gmail.com>
2021-08-23 13:51:30 -0500

    intro and color changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import './intro.scss'
+import React from "react";
+import "./intro.scss";
 
 export default function Intro() {
-    return (
-        <div className="intro" id="intro">
-        </div>
-    )
+  return (
+    <div className="intro" id="intro">
+      <div className="wrapper">
+        <h1>Full Stack Software Developer</h1>
+        <p>My name is Dante Klaus. I am a highly motivated developer seeking to further my career in software development.</p>
+        <p>I have work experience with HTML, CSS, Javascript, Swift, SQL, Nodejs + Express, and AWS.</p>
+      </div>
+      <a href="#portfolio">
+        ⬇️
+      </a>
+    </div>
+  );
 }
